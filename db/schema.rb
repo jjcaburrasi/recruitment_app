e4d0e49cd57939615986bdd57917f2a60397a02b
Silvana Lis Gallo <silvana.lis.g@gmail.com>
2022-08-17 17:36:55 +0200

    Job&Candidate relation created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_170526) do
+ActiveRecord::Schema.define(version: 2022_08_17_085722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2022_08_16_170526) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "jobs_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "job_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
