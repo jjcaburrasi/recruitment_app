3a59b0d0b04774086ec97c0f7c9aa9e9b11e910b
Silvana Lis Gallo <silvana.lis.g@gmail.com>
2022-08-17 18:45:15 +0200

    Device configured to authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def hello
         render html: "Hola mundo"
     end
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        end
 end
