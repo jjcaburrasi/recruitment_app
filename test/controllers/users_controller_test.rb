ea9627aac107180192dbe6568a89dd5a60b25a9c
Silvana Lis Gallo <silvana.lis.g@gmail.com>
2022-08-19 16:51:51 +0200

    Add patch admin permission test and user fixtures

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,16 +1,19 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  include Devise::Test::IntegrationHelpers # Rails >= 5
 
-  # test "should not allow the admin attribute to be edited via the web" do
-  #   log_in_as(@other_user)
-  #   assert_not @other_user.admin?
-  #   patch user_path(@other_user), params: {
-  #                 user: { password: "password",
-  #                 admin: 1 } }
-  #   assert_not @other_user.reload.admin?
-  # end
+  def setup
+    @other_user = users(:silvana)
+    
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    sign_in @other_user
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: "password",
+                                            admin: 1 } }
+    assert_not @other_user.reload.admin?
+  end
 end
