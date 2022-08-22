f85a3cbe9c309441bc58c1646453268ff970c7ad
Silvana Lis Gallo <silvana.lis.g@gmail.com>
2022-08-22 16:47:49 +0200

    Add admin to show jobs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-   
-
     protected
 
         def configure_permitted_parameters
@@ -15,5 +12,4 @@ class ApplicationController < ActionController::Base
         def after_sign_in_path_for(resource)
            @user
         end
-
 end
