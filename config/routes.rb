7f497c626263188d6afcba528abd49f153b97ecf
jjcaburrasi <your.email@example.com>
2022-08-27 16:25:52 +0200

    Working on tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :offers
   devise_for :users
   root 'static_pages#home'
+<<<<<<< Updated upstream
   get  '/kanban',     to: 'jobs#kanban' ##modificarrrrr
   get  '/promote',    to: 'applications#promote' ##modificarrrrr
   get  '/unpromote',  to: 'applications#go_back_promote' #modificat tambieeeeeen
@@ -10,6 +11,14 @@ Rails.application.routes.draw do
   get  '/help',       to: 'static_pages#help'
   get  '/about',      to: 'static_pages#about'
   get  '/jobs',       to: 'jobs#index'
+=======
+  get  '/kanban',   to: 'jobs#kanban'
+  get  '/promote',   to: 'applications#promote'
+  get  '/unpromote', to: 'applications#go_back_promote' 
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/jobs',    to: 'jobs#index'
+>>>>>>> Stashed changes
   resources :users
   resources :jobs
   resources :applications
