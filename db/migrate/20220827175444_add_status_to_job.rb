b46944af7165949ea5c6a76ac075d5c0746f563e
Silvana Lis Gallo <silvana.lis.g@gmail.com>
2022-08-28 13:01:49 +0200

    Add Job Status

--- db/migrate/20220827175444_add_status_to_job.rb
+++ db/migrate/20220827175444_add_status_to_job.rb
@@ -1,5 +1,6 @@
 class AddStatusToJob < ActiveRecord::Migration[6.1]
   def change
-    add_column :jobs, :status, :string, default: "unpublished"; 
+    add_column :jobs, :status, :string, default: "created"; 
+    remove_column :jobs, :published
   end
 end
